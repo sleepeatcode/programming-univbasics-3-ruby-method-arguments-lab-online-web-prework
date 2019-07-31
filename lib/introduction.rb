c23d5337bfd69aba2df0a7292a1888f7673c7ab9
Rachel T <rachmaybe@gmail.com>
2019-07-31 03:30:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,4 +3,4 @@
 #  puts "Hi, my name is #{name}."
 # end
 
-expect{introduction("Josh")}.to output("Hi, my name is Josh.\n").to_stdout
+expect{introduction("Josh")}.to output("Hi, my name is Josh.\n")
