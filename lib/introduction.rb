c6cd60f029c342e0da82ec0aa08183c379eb43bc
Rachel T <rachmaybe@gmail.com>
2019-07-31 03:31:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,6 +1,6 @@
 #introduction 
-#def "introduction_name"
-#  puts "Hi, my name is #{name}."
-# end
+def "introduction_name"
+  puts "Hi, my name is #{name}."
+ end
 
 {introduction("Josh")}.to output("Hi, my name is Josh.\n")
