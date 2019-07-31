b90878e8478926705a9a681ec58dfae5df740d6b
Rachel T <rachmaybe@gmail.com>
2019-07-31 04:00:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 #introduction name, language
-def "introduction_language(name = "w", language)"
+def "introduction_language(name = "w", language = Ruby)"
   puts "Hello, #{name,}, and I am learning to program in #{language}"
  end
 
